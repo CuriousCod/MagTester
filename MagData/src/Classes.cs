0fb32ac3637af0f1dc337f1951459aa099982e35
CuriousCod <69136884+CuriousCod@users.noreply.github.com>
2023-07-09 01:24:40 +0300

    Added rest of the required definitions for player character

--- MagData/src/Classes.cs
+++ MagData/src/Classes.cs
@@ -7,3 +7,25 @@ public enum Classes
     Ranger = 1,
     Force = 2
 }
+
+public enum SectionIDs
+{
+    None = -1,
+    Viridia = 0,
+    Greenill = 1,
+    Skyly = 2,
+    Bluefull = 3,
+    Purplenum = 4,
+    Pinkal = 5,
+    Redria = 6,
+    Oran = 7,
+    Yellowboze = 8,
+    Whitill = 9
+}
+
+public enum Gender
+{
+    None,
+    Male,
+    Female
+}
